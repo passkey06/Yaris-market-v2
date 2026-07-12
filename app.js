b47b024d2eaa2a700a21e63d76fbe5a6ae29971a
passkey06 <passkey1@gmail.com>
2026-07-12 22:49:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 import { db } from "./firebase.js";
 
-console.log("Yarış Market 1.0 başladı.");
+const searchInput = document.getElementById("searchInput");
+const categoryList = document.getElementById("categoryList");
+const campaignList = document.getElementById("campaignList");
+const productList = document.getElementById("productList");
+const cartCount = document.getElementById("cartCount");
+const whatsappBtn = document.getElementById("whatsappBtn");
+
+let cart = [];
+
+cartCount.textContent = "0 Ürün";
+
+whatsappBtn.addEventListener("click", () => {
+    alert("WhatsApp sipariş sistemi bir sonraki adımda eklenecek.");
+});
+
+console.log("Yarış Market 1.0 Hazır");
 console.log(db);
