c1ce82b4f26dd48cef9edd90e8179e4ceb7cd245
passkey06 <passkey1@gmail.com>
2026-07-12 21:56:24 +0300

    Update firabase.js

--- js/firabase.js
+++ js/firabase.js
@@ -1,49 +1,24 @@
-// Firebase Ayarları
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import {
-getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import {
-getStorage
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-import {
-getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-
-apiKey:"AIzaSyClZ4vbfNnWPQi6uTwbJH_6d1TRM_OoBqE",
-
-authDomain:"deneme-a4c1e.firebaseapp.com",
-
-projectId:"deneme-a4c1e",
-
-storageBucket:"deneme-a4c1e.firebasestorage.app",
-
-messagingSenderId:"582311785485",
-
-appId:"1:582311785485:web:42c688aa9ca5de4bead144"
-
+  apiKey: "AIzaSyA_URXNzAUoTB2CC2i1eA82yu_Zqn2BZA8",
+  authDomain: "yarismarketv2.firebaseapp.com",
+  projectId: "yarismarketv2",
+  storageBucket: "yarismarketv2.firebasestorage.app",
+  messagingSenderId: "1004805018905",
+  appId: "1:1004805018905:web:097cb7117ed4d214e22188"
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-
 const storage = getStorage(app);
-
 const auth = getAuth(app);
 
-export {
-
-db,
-
-storage,
-
-auth
-
-};
+export { db, storage, auth };
